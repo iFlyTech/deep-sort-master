b4d48454fefcda082662489ab5c9066d9de99270
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-12 15:29:03 +0000

    7.6.0

--- config/generate.py
+++ config/generate.py
@@ -5,4 +5,5 @@ Parses commmand line arguments for generate.py
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--name", type=str, help="the name of the dataset (e.g. t
+parser.add_argument("--name", type=str, help="the name of the dataset (e.g. train, val, test)")
+parser.add_ar
