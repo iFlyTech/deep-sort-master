c4280e736980fb425b361f4f0859eec243518c98
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-16 20:47:13 +0000

    Add wrapper

--- config/generate.py
+++ config/generate.py
@@ -7,4 +7,4 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", type=str, help="the name of the dataset (e.g. train, val, test)")
 parser.add_argument("--size", type=int, help="size of the dataset to generate")
-parser.add_argument("--max_va
+parser.add_argument("--max_val", type=int, help="the maxi
