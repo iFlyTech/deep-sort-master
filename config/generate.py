0c123d9106498ce2485566c4b242387f220d6072
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-16 22:31:50 +0000

    Tweak wording

--- config/generate.py
+++ config/generate.py
@@ -7,4 +7,5 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", type=str, help="the name of the dataset (e.g. train, val, test)")
 parser.add_argument("--size", type=int, help="size of the dataset to generate")
-parser.add_argument("--max_val", type=int, help="the maxi
+parser.add_argument("--max_val", type=int, help="the maximum value in an array")
+parser.add_
