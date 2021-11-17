c99b78f009eaa28b2dc99e0263e8655d770eda88
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-17 17:07:21 +0000

    Improve README

--- config/generate.py
+++ config/generate.py
@@ -8,4 +8,5 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--name", type=str, help="the name of the dataset (e.g. train, val, test)")
 parser.add_argument("--size", type=int, help="size of the dataset to generate")
 parser.add_argument("--max_val", type=int, help="the maximum value in an array")
-parser.add_argument("--min_length", type=int, help="t
+parser.add_argument("--min_length", type=int, help="the min size of array")
+parser.add_argument("--m
