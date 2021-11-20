9048551887257c58fc3b93e3344b0d5a841b06bd
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-20 18:58:49 +0000

    mm: prevent to write out dirty page in CMA by may_writepage

--- config/generate.py
+++ config/generate.py
@@ -10,4 +10,9 @@ parser.add_argument("--size", type=int, help="size of the dataset to generate")
 parser.add_argument("--max_val", type=int, help="the maximum value in an array")
 parser.add_argument("--min_length", type=int, help="the min size of array")
 parser.add_argument("--max_length", type=int, help="the max size of array")
-parser.add_argument("--ewc", action="store_true", help="generate sequence of dat
+parser.add_argument("--ewc", action="store_true", help="generate sequence of datasets for ewc")
+
+
+def get_config():
+    """
+   
