2eae294697b39f0d33bbb6320889283af1e413d4
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-20 13:04:01 +0000

    Fix linting.

--- config/generate.py
+++ config/generate.py
@@ -10,4 +10,4 @@ parser.add_argument("--size", type=int, help="size of the dataset to generate")
 parser.add_argument("--max_val", type=int, help="the maximum value in an array")
 parser.add_argument("--min_length", type=int, help="the min size of array")
 parser.add_argument("--max_length", type=int, help="the max size of array")
-parser.add_argument("--ewc", actio
+parser.add_argument("--ewc", action="store_true", help="generate sequence of dat
