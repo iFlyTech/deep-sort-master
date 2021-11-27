ed546a1c8928069f8c399333bacad07cabb20cb0
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-27 11:34:13 +0000

    Update rast.coffee

--- evaluate.py
+++ evaluate.py
@@ -20,3 +20,30 @@ def evaluate(encoder, decoder, input_tensor, is_ptr, max_length=MAX_LENGTH):
     with torch.no_grad():
         input_length = input_tensor.size()[0]
         encoder_hidden = encoder.init_hidden()
+
+        encoder_outputs = torch.zeros(input_length, encoder.hidden_dim, device=device)
+
+        for i in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[i], encoder_hidden)
+            encoder_outputs[i] += encoder_output[0, 0]
+
+        decoder_input, decoder_hidden = torch.tensor([[SOS_token]], device=device), encoder_hidden
+
+        decoded_output = []
+
+        for i in range(max_length):
+            args = (decoder_input, decoder_hidden, encoder_outputs)
+            if is_ptr:
+                args += (input_tensor,)
+            decoder_output, decoder_hidden, decoder_attention = decoder(*args)
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_output.append('<EOS>')
+                break
+            else:
+                decoded_output.append(topi.item())
+
+            # detach from history as input
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_output
