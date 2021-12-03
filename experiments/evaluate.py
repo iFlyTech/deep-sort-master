58494aaee2c14a05c0b80366f1b0df11ef60ab86
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-12-03 18:25:19 +0000

    tests ok

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -8,4 +8,9 @@ from evaluate import evaluate
 from models.attn_decoder import AttnDecoder
 from models.encoder import Encoder
 from models.ptr_decoder import PtrDecoder
-from utils import load_checkpoint, devi
+from utils import load_checkpoint, device, RANDOM_SEED
+
+
+def run():
+    """
+    Run th
