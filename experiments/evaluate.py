0c91df6ee5cbc017b0f8df504b2a28b07a11f747
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-12-03 13:20:28 +0000

    Fix publish

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -7,4 +7,5 @@ from data import read_data, tensors_from_pair
 from evaluate import evaluate
 from models.attn_decoder import AttnDecoder
 from models.encoder import Encoder
-from models.ptr_decoder import Pt
+from models.ptr_decoder import PtrDecoder
+from utils import load_checkpoint, devi
