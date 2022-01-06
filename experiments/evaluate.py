577d3c48178d0b3d1cef01debacff76620c4a0d7
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-01-06 09:37:12 +0000

    Fixed linter errors

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -49,4 +49,4 @@ def run():
                                  decoder=decoder,
                                  input_tensor=training_pairs[i][0],
                                  is_ptr=is_ptr)
- 
+        print(list(np.asarray(input_tensor.
