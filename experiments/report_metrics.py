7b95390097d934a6526dcfa0f22f47b9c9285990
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-01-08 05:47:21 +0000

    drm/radeon: add new richland pci ids

--- experiments/report_metrics.py
+++ experiments/report_metrics.py
@@ -17,3 +17,45 @@ def run():
     """
     Run the experiment.
     """
+    is_ptr = False
+    np.random.seed(RANDOM_SEED)
+    max_val, max_length, pairs = read_data(name="test")
+    np.random.shuffle(pairs)
+    training_pairs = [tensors_from_pair(pair) for pair in pairs]
+
+    data_dim = max_val + 1
+    hidden_dim = embedding_dim = 256
+
+    encoder = Encoder(input_dim=data_dim,
+                      embedding_dim=embedding_dim,
+                      hidden_dim=hidden_dim).to(device)
+    if is_ptr:
+        decoder = PtrDecoder(output_dim=data_dim,
+                             embedding_dim=embedding_dim,
+                             hidden_dim=hidden_dim).to(device)
+    else:
+        decoder = AttnDecoder(output_dim=data_dim,
+                              embedding_dim=embedding_dim,
+                              hidden_dim=hidden_dim).to(device)
+
+    checkpoint = load_checkpoint("ptr" if is_ptr else "vanilla")
+    if checkpoint:
+        encoder.load_state_dict(checkpoint["encoder"])
+        decoder.load_state_dict(checkpoint["decoder"])
+    else:
+        print("Count not find checkpoint file.")
+
+    permutation_count, nondecreasing_count = 0, 0
+    for i in range(len(training_pairs)):
+        input_tensor, target_tensor = training_pairs[i]
+        output_tensor = evaluate(encoder=encoder,
+                                 decoder=decoder,
+                                 input_tensor=training_pairs[i][0],
+                                 is_ptr=is_ptr)
+        target, output = list(np.asarray(input_tensor.data).squeeze()), output_tensor[:-1]
+        if is_permutation(target, output):
+            permutation_count += 1
+        if nondecreasing(output) == 0:
+            nondecreasing_count += 1
+    print("Permutation: %s" % (permutation_count / len(training_pairs)))
+    print("Nondecreasing: %s" % (nondecreasing_count / len(training_pairs)))
---
+++ experiments/train.py
@@ -0,0 +1,4 @@
+
+"""
+Train the network.
+"""
