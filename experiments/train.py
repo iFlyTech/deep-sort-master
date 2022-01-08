2115f8a8e989b9609987b9ae618147dc36524921
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-01-08 10:04:52 +0000

    Bump version to 0.6

--- experiments/train.py
+++ experiments/train.py
@@ -2,3 +2,42 @@
 """
 Train the network.
 """
+
+import torch.nn as nn
+from torch import optim
+
+from data import read_data, tensors_from_pair
+from models.attn_decoder import AttnDecoder
+from models.encoder import Encoder
+from models.ptr_decoder import PtrDecoder
+from train import train
+from utils import device, set_max_length
+
+
+def weight_init(module):
+    """
+    Initialize weights of <module>. Applied recursivly over model weights via .apply()
+    """
+    for parameter in module.parameters():
+        nn.init.uniform_(parameter, -0.08, 0.08)
+
+
+def run():
+    """
+    Run the experiment.
+    """
+    name = "train"
+    is_ptr = True
+    hidden_dim = embedding_dim = 256
+    n_epochs = 1
+    grad_clip = 2
+    teacher_force_ratio = 0.5
+    optimizer = optim.Adam
+    optimizer_params = {}
+
+    max_val, max_length, pairs = read_data(name)
+
+    set_max_length(max_length)
+    training_pairs = [tensors_from_pair(pair) for pair in pairs]
+
+    data_dim = max_val + 1
