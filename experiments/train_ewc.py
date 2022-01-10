ca9a67b535d4e6aaf6f4eb8b9b67a648989b463e
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-01-10 17:27:16 +0000

    perm

--- experiments/train_ewc.py
+++ experiments/train_ewc.py
@@ -4,4 +4,5 @@ Simple experiment testing EWC training procedure.
 import torch.nn as nn
 from torch import optim
 
-from data import read_da
+from data import read_data, tensors_from_pair
+from models.e
