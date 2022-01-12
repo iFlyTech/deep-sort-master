c3afeafe71a99d82e0906cae3c409fa0bcaa85c2
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-01-12 12:18:58 +0000

    Add JUnit tests.

--- experiments/train_ewc.py
+++ experiments/train_ewc.py
@@ -6,4 +6,5 @@ from torch import optim
 
 from data import read_data, tensors_from_pair
 from models.encoder import Encoder
-from models.ptr_decoder im
+from models.ptr_decoder import PtrDecoder
+from train_ewc import t
