af54e58509a33790cba4b718c54f03618ad4c56e
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-01-14 07:25:24 +0000

    pass

--- experiments/train_ewc.py
+++ experiments/train_ewc.py
@@ -7,4 +7,8 @@ from torch import optim
 from data import read_data, tensors_from_pair
 from models.encoder import Encoder
 from models.ptr_decoder import PtrDecoder
-from train_ewc import t
+from train_ewc import train
+from utils import device, set_max_length
+
+
+de
