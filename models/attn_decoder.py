aa9e249a6efadf76dee3678cb22db89012f47f46
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-03-15 06:00:00 +0000

    add shell

--- models/attn_decoder.py
+++ models/attn_decoder.py
@@ -4,4 +4,5 @@ The attention decoder model.
 Modified from https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
 """
 import torch
-import tor
+import torch.nn as nn
+import torch.nn.functional 
