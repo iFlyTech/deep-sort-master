8aadba249702d4552f32cf3033ce72d94240800e
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-04-14 20:25:08 +0000

    new file

--- models/attn_decoder.py
+++ models/attn_decoder.py
@@ -28,4 +28,5 @@ class AttnDecoder(nn.Module):
 
         self.embedding = nn.Embedding(num_embeddings=self.output_dim,
                                       embedding_dim=embedding_dim)
-        self.attn = nn.Linear(in_featur
+        self.attn = nn.Linear(in_features=self.hidden_dim + embedding_dim,
+                     
