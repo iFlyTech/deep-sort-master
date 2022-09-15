40e6207300746af886073dc8bb6738c03a1b9ac1
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-09-15 21:35:33 +0000

    Update the submodule: anthos-service-mesh-packages

--- tests/metrics.py
+++ tests/metrics.py
@@ -5,4 +5,5 @@ from metrics import is_permutation, nondecreasing
 
 
 def test_permutation():
-    a, b = [1, 2, 3, 4], [1, 
+    a, b = [1, 2, 3, 4], [1, 2, 3, 4, 5]
+    assert not is_permutation(a, b)
