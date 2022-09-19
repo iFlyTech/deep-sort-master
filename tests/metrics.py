765b5b4c161cd71395039856d500240b57e97592
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-09-19 07:36:53 +0000

    Add bower

--- tests/metrics.py
+++ tests/metrics.py
@@ -7,3 +7,5 @@ from metrics import is_permutation, nondecreasing
 def test_permutation():
     a, b = [1, 2, 3, 4], [1, 2, 3, 4, 5]
     assert not is_permutation(a, b)
+    a, b = [1, 2, 3, 4], [1, 2, 3, 4]
+    assert is_pe
