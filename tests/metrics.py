cb382055e1360a2e2918276340c278d28ec157f3
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-10-09 07:54:23 +0000

    Solve conflicts

--- tests/metrics.py
+++ tests/metrics.py
@@ -10,4 +10,6 @@ def test_permutation():
     a, b = [1, 2, 3, 4], [1, 2, 3, 4]
     assert is_permutation(a, b)
     a, b = [4, 3, 2, 1], [1, 2, 3, 4]
-    assert is_permutation(a, b
+    assert is_permutation(a, b)
+    a, b = [1, 3, 2, 4], [3, 2, 4, 1]
+    asse
