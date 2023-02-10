1a8d7cccf2a006b8f2ee572426dec111732d44a4
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2023-02-10 06:22:27 +0000

    Added GPLv3 license

--- train.py
+++ train.py
@@ -24,3 +24,4 @@ def train(encoder, decoder, optim, optim_params, weight_init, grad_clip, is_ptr,
     np.random.seed(RANDOM_SEED), torch.manual_seed(RANDOM_SEED)
     encoder.train(), decoder.train()
     encoder_optim = optim(encoder.parameters(), **optim_params)
+    decoder_optim = optim(decoder.parame
