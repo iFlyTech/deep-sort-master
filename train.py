48450d13dee9a5ed297a1572d8fa542d7d0f15fe
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2023-05-28 22:33:50 +0000

    Sound Control: 3.6

--- train.py
+++ train.py
@@ -120,4 +120,4 @@ def train_step(training_pair, encoder, decoder, encoder_optim, decoder_optim, is
     teacher_force = random.random() < teacher_force_ratio
 
     for i in range(target_length):
-      
+        args = (decoder_input, decoder_hidden, enco
