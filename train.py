95421b3f45978f8cdbeba2898b8683953451cc48
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2023-06-02 17:10:24 +0000

    alphabetical order

--- train.py
+++ train.py
@@ -130,4 +130,4 @@ def train_step(training_pair, encoder, decoder, encoder_optim, decoder_optim, is
             decoder_input = topi.squeeze().detach()
         else:
             decoder_input = target_tensor[i]
- 
+        loss += criterion(decoder_output, target_ten
