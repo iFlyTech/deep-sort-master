26d1123592fed37f28f6ca485e2db6b2d6b7cbee
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2023-06-14 10:22:41 +0000

    updating index.html

--- utils.py
+++ utils.py
@@ -47,3 +47,37 @@ def time_since(since, percent):
     es = s / percent
     rs = es - s
     return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
+
+
+def save_checkpoint(state, model):
+    """
+    Save a checkpoint of the current model weights and optimizer state.
+    """
+    epoch, iteration = state["epoch"], state["iter"]
+    root_dir = os.path.join("checkpoints", model)
+    file_name = os.path.join(root_dir, "e" + str(epoch) + "i" + str(iteration) + ".ckpt")
+    if not os.path.isdir(root_dir):
+        os.mkdir(root_dir)
+    torch.save(state, file_name)
+
+
+def load_checkpoint(model):
+    """
+    Load the most recent (i.e. greatest number of iterations) checkpoint file.
+    """
+    root_dir = os.path.join("checkpoints", model)
+    if os.path.isdir(root_dir):
+        max_epoch, max_iteration = -1, -1
+        argmax_file = ""
+        for file_name in os.listdir(root_dir):
+            try:
+                pattern = re.compile(r"""e(?P<epoch>[\d]*)
+                                         i(?P<iter>[\d]*)
+                                         \.ckpt""", re.VERBOSE)
+                match = pattern.match(file_name)
+                epoch, iteration = int(match.group("epoch")), int(match.group("iter"))
+                if epoch > max_epoch and iteration > max_iteration:
+                    max_epoch, max_iteration = epoch, iteration
+                    argmax_file = file_name
+            except (ValueError, AttributeError):
+                print("A file other than a checkpoint appears to be in the checkpoints folder; please remove it")
